d183102988a887df4670a3d9a67bfd5543b465f5
tancredi_danna <tancredidanna5@gmail.com>
2021-10-07 14:29:35 +0200

    primo esercizio

--- app/src/main/java/com/example/check_birth/MainActivity.kt
+++ app/src/main/java/com/example/check_birth/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.check_birth
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.check_birth.databinding.ActivityMainBinding
+import java.time.Year
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mBinding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.materialButton.setOnClickListener {
+            mBinding.textView3.text =""
+            try{
+                val birthDate = mBinding.editTextDate.text.toString().toInt()
+                findAge(birthDate)
+
+            }catch (e:NumberFormatException){
+
+            }
+
+
+
+        }
+    }
+    private fun findAge(birth:Int){
+        val year = Calendar.getInstance().get(Calendar.YEAR)
+        val age = year-birth
+        mBinding.textView3.text = " Your age is: ${age.toString()}"
+
+
     }
 }
